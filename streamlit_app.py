47c32a5613228575e6757b8fedcbe23f792d9dfb
Andrews Cordolino Sobral <andrewssobral@users.noreply.github.com>
2023-03-02 20:53:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,58 @@
-from collections import namedtuple
-import altair as alt
-import math
+import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+from streamlit_autorefresh import st_autorefresh
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+if 'pre_selected_rows' not in st.session_state:
+    st.session_state.pre_selected_rows = []
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+df = pd.DataFrame(columns=['ID', 'STATUS'])
+df['ID'] = [1, 2, 3]
+df['STATUS'] = np.random.randint(0,100,size=(3))
+st.write(df)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# get pre-selected rows from session state
+pre_selected_rows = st.session_state.pre_selected_rows
+st.write("pre_selected_rows: ", pre_selected_rows)
 
+# use the pre-selected rows when building the grid options
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_default_column(editable=True, wrapText=True, autoHeight=True)
+gb.configure_column('ID', minWidth=80, maxWidth=80, type=["numericColumn","numberColumnFilter"], sortable=True, sort='asc', headerCheckboxSelection=True)
+gb.configure_column('STATUS', minWidth=100, maxWidth=100)
+gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=3)
+gb.configure_side_bar()
+gb.configure_selection('multiple', pre_selected_rows=pre_selected_rows, rowMultiSelectWithClick=False, use_checkbox=True)
+gb.configure_grid_options(suppressRowHoverHighlight=True, columnHoverHighlight=False, pre_selected_rows=pre_selected_rows)
+gb_grid_options = gb.build()
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# render the grid and get the selected rows
+grid_return = AgGrid(
+    df,
+    gridOptions = gb_grid_options,
+    key = 'ID',
+    reload_data = True,
+    data_return_mode = DataReturnMode.AS_INPUT,
+    update_mode = GridUpdateMode.MODEL_CHANGED,
+    allow_unsafe_jscode = True,
+    fit_columns_on_grid_load = False,
+    enable_enterprise_modules = False,
+    height = 320,
+    width = '100%',
+    theme = "streamlit"
+)
+selected_rows = grid_return["selected_rows"]
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# print the selected rows
+st.write("Selected rows: ", selected_rows)
 
-    points_per_turn = total_points / num_turns
+# save the row indexes of the selected rows in the session state
+pre_selected_rows = []
+for selected_row in selected_rows:
+        pre_selected_rows.append(selected_row['_selectedRowNodeInfo']['nodeRowIndex'])
+st.session_state.pre_selected_rows = pre_selected_rows
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Refresh streamlit components every 1s
+st_autorefresh(interval=((1*1*1000)), key="dataframerefresh")
